ce8b9122786d27e0b0606d3bc1a66c7e5dff7c06
Raymond <raymond07065@gmail.com>
2023-08-20 11:46:49 +0800

    Update column name to describe value better. Added simple estimate logic

--- electricity-monitor/estimated-bill/script.js
+++ electricity-monitor/estimated-bill/script.js
@@ -4,28 +4,30 @@
 const billTableBody = document.getElementById('bill-table');
 
 // Function to calculate the total cost
-function calculateTotalCost(consumption, rate) {
-  return (consumption * rate).toFixed(2);
+function calculateTotalCost(group, rate) {
+  const daysInMonth = getDaysInMonth(group.year,group.month);  
+  const multiples = daysInMonth * 24 / group.hoursDifference;
+  
+  return (multiples * (group.lastValue - group.firstValue) * rate).toFixed(2);
 }
 
 // Populate the Estimated Bill table with input history
 function populateEstimatedBillTable(inputHistory) {
   const rate = 0.2775; // Electricity rate per kWh
-  var estimates = groupData(inputHistory);
-  console.log(estimates);
+  var estimates = groupDataByMonth(inputHistory);
 
-  estimates.forEach(entry => {    
+  estimates.forEach(group => {
     var date = new Date();
-    date.setMonth(entry.month);
-    const month = date.toLocaleString('en-US', { month: 'long' });
 
-    const consumption = entry.sum;        
-    const totalCost = calculateTotalCost(consumption, rate);
+    date.setMonth(group.month);
+    const month = date.toLocaleString('en-US', { month: 'long' });
+    
+    const totalCost = calculateTotalCost(group, rate);
 
     const row = document.createElement('tr');
     row.innerHTML = `
-      <td>${month} - ${entry.year}</td>
-      <td>${consumption.toFixed(1)} kWh</td>
+      <td>${month} - ${group.year}</td>
+      <td> kWh</td>
       <td>${rate.toFixed(4)} $/kWh</td>
       <td>${totalCost} $</td>
     `;
@@ -34,7 +36,7 @@ function populateEstimatedBillTable(inputHistory) {
   });
 }
 
-function groupData(data){
+function groupDataByMonth(data) {
   const groupedData = {};
 
   data.forEach(item => {
@@ -44,13 +46,51 @@ function groupData(data){
     const key = `${year}-${month}`;
 
     if (!groupedData[key]) {
-      groupedData[key] = { year, month, sum: 0 };
+      groupedData[key] = { year: year, month: month};
+    }
+    const group = groupedData[key];
+
+    if (!group.firstDay) {
+      group.firstDay = timestamp;
+      group.firstValue = item.value;
+    }
+    else {
+      group.lastDay = timestamp;
+      group.lastValue = item.value;
     }
 
-    groupedData[key].sum += parseFloat(item.value);
+    const hoursDifference = calculateHourDifference(group.firstDay, group.lastDay);
+    if (hoursDifference) {
+      group.hoursDifference = hoursDifference;            
+    }
+
   });
 
-  return Object.values(groupedData);    
+  return Object.values(groupedData);
+}
+
+function calculateHourDifference(date1, date2) {
+  if (!date1 || !date2) {
+    return null;
+  }
+  // Calculate the time difference in milliseconds
+  const timeDifference = date2 - date1;
+
+  // Convert the time difference to days
+  const hoursDifference = Math.round(timeDifference / (1000 * 60 * 60));
+  return hoursDifference;
+}
+
+function getDaysInMonth(year, month) {
+  // Create a new Date object for the given year and month (months are 0-indexed)
+  const date = new Date(year, month, 1);
+
+  // Move to the next month and subtract 1 day to get the last day of the target month
+  date.setMonth(date.getMonth() + 1);
+  date.setDate(date.getDate() - 1);
+
+  // The date now holds the last day of the target month, which gives the number of days in that month
+  return date.getDate();
 }
 
 // Simulated input history for testing
